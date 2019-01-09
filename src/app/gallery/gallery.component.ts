3ccfb7fd53e13c9fc331c1d0a5e9249f8ab34795
Gagulo <grzegorz.sokalski@hotmail.com>
2019-01-09 13:56:27 +0100

    getting to photos

--- src/app/gallery/gallery.component.ts
+++ src/app/gallery/gallery.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ImageService } from '../shared/image.service';
 
 @Component({
   selector: 'app-gallery',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GalleryComponent implements OnInit {
 
-  constructor() { }
+  title = 'Recent Photo'
+  visibleImages: any[] = [];
+
+  constructor(private imageService: ImageService) {
+    this.visibleImages = this.imageService.getImages();
+   }
 
   ngOnInit() {
   }
